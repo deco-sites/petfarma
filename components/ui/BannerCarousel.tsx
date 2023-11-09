79fb2171c723e846ab48ee49ed851a2d1fd0cf5d
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-11-09 14:56:55 -0300

    feat: change the layout in BannerCarousel and changes the chevron sprites

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -20,16 +20,10 @@ export interface Banner {
   mobile: ImageWidget;
   /** @description Image's alt text */
   alt: string;
-  action?: {
-    /** @description when user clicks on the image, go to this link */
-    href: string;
-    /** @description Image text title */
-    title: string;
-    /** @description Image text subtitle */
-    subTitle: string;
-    /** @description Button label */
-    label: string;
-  };
+  /** @description when user clicks on the image, go to this link */
+  href: string;
+  /** @description Button label */
+  label: string;
 }
 
 export interface Props {
@@ -49,42 +43,12 @@ const DEFAULT_PROPS = {
   images: [
     {
       alt: "/feminino",
-      action: {
-        href: "https://www.deco.cx/",
-        label: "deco.cx",
-        title: "Demo Store",
-        subTitle: "Visit our site and start building now:",
-      },
+      href: "https://www.deco.cx/",
+      label: "deco.cx",
       mobile:
         "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/24278f9e-412d-4a8a-b2d3-57353bb1b368",
       desktop:
         "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/afa2c07c-74f4-496d-8647-5cc58f48117b",
-    },
-    {
-      alt: "/feminino",
-      action: {
-        href: "https://www.deco.cx/",
-        label: "deco.cx",
-        title: "Demo Store",
-        subTitle: "Visit our site and start building now:",
-      },
-      mobile:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/eeaa624c-a3e1-45e8-a6fe-034233cfbcd0",
-      desktop:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/7949d031-9a79-4639-b85e-62fd90af85a9",
-    },
-    {
-      alt: "/feminino",
-      action: {
-        href: "https://www.deco.cx/",
-        label: "deco.cx",
-        title: "Demo Store",
-        subTitle: "Visit our site and start building now:",
-      },
-      mobile:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/ae89571c-4a7c-44bf-9aeb-a341fd049d19",
-      desktop:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/7ec121e4-5cfe-4b7b-b942-d1ed4493803d",
     },
   ],
   preload: true,
@@ -97,15 +61,16 @@ function BannerItem(
     alt,
     mobile,
     desktop,
-    action,
+    href,
+    label,
   } = image;
 
   return (
     <a
       id={id}
-      href={action?.href ?? "#"}
-      aria-label={action?.label}
-      class="relative h-[600px] overflow-y-hidden w-full"
+      href={href ?? "#"}
+      aria-label={label}
+      class="relative h-[400px] overflow-y-hidden w-full max-w-[1312px]"
     >
       <Picture preload={lcp}>
         <Source
@@ -113,33 +78,22 @@ function BannerItem(
           fetchPriority={lcp ? "high" : "auto"}
           src={mobile}
           width={360}
-          height={600}
+          height={400}
         />
         <Source
           media="(min-width: 768px)"
           fetchPriority={lcp ? "high" : "auto"}
           src={desktop}
           width={1440}
-          height={600}
+          height={400}
         />
         <img
-          class="object-cover w-full h-full"
+          class="object-cover w-full h-full rounded-lg"
           loading={lcp ? "eager" : "lazy"}
           src={desktop}
           alt={alt}
         />
       </Picture>
-      {action && (
-        <div class="absolute h-min top-0 bottom-0 m-auto left-0 right-0 sm:right-auto sm:left-[12%] max-h-min max-w-[400px] flex flex-col gap-4 p-4 rounded glass">
-          <span class="text-6xl font-medium text-base-100">
-            {action.title}
-          </span>
-          <span class="font-medium text-xl text-base-100">
-            {action.subTitle}
-          </span>
-          <Button class="glass">{action.label}</Button>
-        </div>
-      )}
     </a>
   );
 }
@@ -158,17 +112,15 @@ function Dots({ images, interval = 0 }: Props) {
           `,
         }}
       />
-      <ul class="carousel justify-center col-span-full gap-4 z-10 row-start-4">
+      <ul class="carousel justify-center col-span-full gap-4 z-10">
         {images?.map((_, index) => (
           <li class="carousel-item">
-            <Slider.Dot index={index}>
-              <div class="py-5">
-                <div
-                  class="w-16 sm:w-20 h-0.5 rounded group-disabled:animate-progress bg-gradient-to-r from-base-100 from-[length:var(--dot-progress)] to-[rgba(255,255,255,0.4)] to-[length:var(--dot-progress)]"
-                  style={{ animationDuration: `${interval}s` }}
-                />
-              </div>
-            </Slider.Dot>
+            <Slider.Dot
+              index={index}
+              class={`${
+                index === 0 && "bg-primary"
+              } w-[42px] bg-gray-200 h-2 rounded-full`}
+            />
           </li>
         ))}
       </ul>
@@ -179,20 +131,20 @@ function Dots({ images, interval = 0 }: Props) {
 function Buttons() {
   return (
     <>
-      <div class="flex items-center justify-center z-10 col-start-1 row-start-2">
-        <Slider.PrevButton class="btn btn-circle glass">
+      <div class="absolute top-1/2 bottom-1/2 -left-7 flex items-center justify-center z-10 col-start-1">
+        <Slider.PrevButton class="btn btn-circle bg-[#fcf7f7]">
           <Icon
-            class="text-base-100"
+            class="text-black"
             size={24}
             id="ChevronLeft"
             strokeWidth={3}
           />
         </Slider.PrevButton>
       </div>
-      <div class="flex items-center justify-center z-10 col-start-3 row-start-2">
-        <Slider.NextButton class="btn btn-circle glass">
+      <div class="absolute top-1/2 bottom-1/2 -right-7 flex items-center justify-center z-10 col-start-3">
+        <Slider.NextButton class="btn btn-circle bg-[#fcf7f7]">
           <Icon
-            class="text-base-100"
+            class="text-black"
             size={24}
             id="ChevronRight"
             strokeWidth={3}
@@ -210,33 +162,37 @@ function BannerCarousel(props: Props) {
   return (
     <div
       id={id}
-      class="grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px]"
+      class="flex flex-col max-w-[1400px] w-full m-auto py-8"
     >
-      <Slider class="carousel carousel-center w-full col-span-full row-span-full gap-6">
-        {images?.map((image, index) => {
-          const params = { promotion_name: image.alt };
+      <div class="relative h-fit">
+        <Slider class="carousel carousel-center w-full col-span-full row-span-full gap-6">
+          {images?.map((image, index) => {
+            const params = { promotion_name: image.alt };
 
-          return (
-            <Slider.Item index={index} class="carousel-item w-full">
-              <BannerItem
-                image={image}
-                lcp={index === 0 && preload}
-                id={`${id}::${index}`}
-              />
-              <SendEventOnClick
-                id={`${id}::${index}`}
-                event={{ name: "select_promotion", params }}
-              />
-              <SendEventOnView
-                id={`${id}::${index}`}
-                event={{ name: "view_promotion", params }}
-              />
-            </Slider.Item>
-          );
-        })}
-      </Slider>
-
-      <Buttons />
+            return (
+              <Slider.Item
+                index={index}
+                class="carousel-item w-full max-w-[1312px]"
+              >
+                <BannerItem
+                  image={image}
+                  lcp={index === 0 && preload}
+                  id={`${id}::${index}`}
+                />
+                <SendEventOnClick
+                  id={`${id}::${index}`}
+                  event={{ name: "select_promotion", params }}
+                />
+                <SendEventOnView
+                  id={`${id}::${index}`}
+                  event={{ name: "view_promotion", params }}
+                />
+              </Slider.Item>
+            );
+          })}
+        </Slider>
+        <Buttons />
+      </div>
 
       <Dots images={images} interval={interval} />
 
